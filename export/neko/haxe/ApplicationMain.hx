45e43346dd43056e82b88281e710f64858d37eae
Nikita Petrov <local_kings@ctemplar.com>
2021-12-06 16:35:18 +1000

    Builded again

--- export/neko/haxe/ApplicationMain.hx
+++ export/neko/haxe/ApplicationMain.hx
@@ -32,7 +32,7 @@ class ApplicationMain
 
 		ManifestResources.init(config);
 
-		app.meta["build"] = "4";
+		app.meta["build"] = "1";
 		app.meta["company"] = "HaxeFlixel";
 		app.meta["file"] = "SimpleServer";
 		app.meta["name"] = "SimpleServer";
